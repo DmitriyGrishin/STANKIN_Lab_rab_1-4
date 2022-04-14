dac2f1a4825c742edeed2420061d09e00acc5d6e
Dmitry <55159672+DmitriyGrishin@users.noreply.github.com>
2022-04-14 09:37:03 +0300

    Update create.php

--- php/create.php
+++ php/create.php
@@ -4,9 +4,9 @@ require_once "connect.php";
 
 $surname = $_POST['surname'];
 $name = $_POST['name'];
-$group = $_POST['group'];
+$group = $_POST['grp'];
 
-$query = "INSERT INTO `form` (`id`, `surname`, `name`, `grp`) VALUES (NULL, '$surname', '$name', '$group')";
+$query = "INSERT INTO `form` (`id`, `surname`, `name`, `grp`) VALUES (NULL, '$surname', '$name', '$grp')";
     if ($conn->query($query) === TRUE)
     {
     $conn->close();
